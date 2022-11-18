547f8bcd4be00589443d4f17c70da40f034289db
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 15:50:37 +0100

    Add translations

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+use EscolaLms\ModelFields\Enum\MetaFieldPermissionsEnum;
+
+return [
+    MetaFieldPermissionsEnum::METADATA_CREATE_UPDATE => 'Create/Update metadata',
+    MetaFieldPermissionsEnum::METADATA_DELETE => 'Delete metadata',
+    MetaFieldPermissionsEnum::METADATA_LIST => 'Metadata list',
+];
---
+++ resources/lang/pl/permissions.php
@@ -0,0 +1,9 @@
+<?php
+
+use EscolaLms\ModelFields\Enum\MetaFieldPermissionsEnum;
+
+return [
+    MetaFieldPermissionsEnum::METADATA_CREATE_UPDATE => 'Utwórz zapisz metadane',
+    MetaFieldPermissionsEnum::METADATA_DELETE => 'Usuń metadane',
+    MetaFieldPermissionsEnum::METADATA_LIST => 'Lista metadanych',
+];
