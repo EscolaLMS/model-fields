590060e9265d48f940bab9111bd6fa8d5b55b15f
Hubert Krzysztofiak <hubert.krzysztofiak@escolasoft.com>
2023-01-10 08:46:36 +0100

    Add condition for passport

--- src/AuthServiceProvider.php
+++ src/AuthServiceProvider.php
@@ -5,6 +5,7 @@ namespace EscolaLms\ModelFields;
 use EscolaLms\ModelFields\Models\Metadata;
 use EscolaLms\ModelFields\Policies\MetadataPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -15,5 +16,8 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
+        if (!$this->app->routesAreCached()) {
+            Passport::routes();
+        }
     }
 }
